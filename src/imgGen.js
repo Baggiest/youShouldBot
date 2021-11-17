dcf5917471be0e509055b1af1ad0c32914d94f8f
mrbaggiebug <mani.ehsanisohi@gmail.com>
2021-11-17 19:17:00 +0330

    ayyy basic shit done

--- src/imgGen.js
+++ src/imgGen.js
@@ -2,7 +2,7 @@ var Jimp = require('jimp');
 
 //random file name generator that i totally didnt yank from stackoverflow
 
-var fileId = (length) =>{
+var fileId = (length) => {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
@@ -12,19 +12,21 @@ var fileId = (length) =>{
     }
     return result;
 }
-console.log(fileId(10));
 
 const imageGen = async () => {
-
+    let fileName = fileId(16);
+    const font = await Jimp.loadFont("../TAHOMABD.fnt");
     // open a file called "lenna.png"
     Jimp.read('../ysk.jpg', (err, ysk) => {
         if (err) throw err;
         ysk
-            .resize(256, 256) // resize
-            .quality(60) // set JPEG quality
-            .greyscale() // set greyscale
-            .write(`../data/${fileId(16)}.jpeg`); // save
+            //.resize(256, 256) // resize
+            .print(font, 100, 0, 'kill yourself')
+            // .quality(60) // set JPEG quality
+            // .greyscale() // set greyscale
+            .write(`../data/${fileName}.jpeg`); // save
     });
+    console.log(fileName)
 }
 
 module.exports = imageGen;
